6f1042af645d25bade271f72aeacb2f069a1a7a8
AMK1215 <aungmyokhaing1215@gmail.com>
2024-11-29 10:18:58 +0630

    user api

--- app/Console/Commands/SyncUsersFromAPI.php
+++ app/Console/Commands/SyncUsersFromAPI.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class SyncUsersFromAPI extends Command
 {
@@ -39,7 +40,7 @@ class SyncUsersFromAPI extends Command
             $apiUsers = $response->json('data');
 
             // Chunk the API data into smaller batches
-            $chunkSize = 500; // Adjust chunk size based on your system's capacity
+            $chunkSize = 100; // Adjust chunk size based on your system's capacity
             $chunks = array_chunk($apiUsers, $chunkSize);
 
             foreach ($chunks as $chunk) {
@@ -54,28 +55,54 @@ class SyncUsersFromAPI extends Command
                     }
 
                     // If user does not exist, create a new user
-                    User::create([
-                        'id' => $apiUser['id'],
-                        'user_name' => $apiUser['user_name'],
-                        'name' => $apiUser['name'],
-                        'phone' => $apiUser['phone'],
-                        'email' => $apiUser['email'],
-                        'email_verified_at' => $apiUser['email_verified_at'],
-                        'profile' => $apiUser['profile'],
-                        'max_score' => $apiUser['max_score'],
-                        'status' => $apiUser['status'],
-                        'is_changed_password' => $apiUser['is_changed_password'],
-                        'agent_id' => $apiUser['agent_id'],
-                        'payment_type_id' => $apiUser['payment_type_id'],
-                        'agent_logo' => $apiUser['agent_logo'],
-                        'account_name' => $apiUser['account_name'],
-                        'account_number' => $apiUser['account_number'],
-                        'line_id' => $apiUser['line_id'],
-                        'commission' => $apiUser['commission'],
-                        'referral_code' => $apiUser['referral_code'],
-                        'created_at' => $apiUser['created_at'],
-                        'updated_at' => $apiUser['updated_at'],
-                    ]);
+                    User::updateOrCreate(
+    ['user_name' => $apiUser['user_name']], // Match condition
+    [
+        'id' => $apiUser['id'],
+        'name' => $apiUser['name'],
+        'phone' => $apiUser['phone'],
+        'email' => $apiUser['email'],
+        'email_verified_at' => $apiUser['email_verified_at'],
+        'profile' => $apiUser['profile'],
+        'max_score' => $apiUser['max_score'],
+        'status' => $apiUser['status'],
+        'is_changed_password' => $apiUser['is_changed_password'],
+        'agent_id' => $apiUser['agent_id'],
+        'payment_type_id' => $apiUser['payment_type_id'],
+        'agent_logo' => $apiUser['agent_logo'],
+        'account_name' => $apiUser['account_name'],
+        'account_number' => $apiUser['account_number'],
+        'line_id' => $apiUser['line_id'],
+        'commission' => $apiUser['commission'],
+        'referral_code' => $apiUser['referral_code'],
+        'password' => Hash::make('delightmyanmar'), // Set a default password if new
+        'created_at' => $apiUser['created_at'],
+        'updated_at' => $apiUser['updated_at'],
+    ]
+);
+
+                    // User::create([
+                    //     'id' => $apiUser['id'],
+                    //     'user_name' => $apiUser['user_name'],
+                    //     'name' => $apiUser['name'],
+                    //     'phone' => $apiUser['phone'],
+                    //     'email' => $apiUser['email'],
+                    //     'email_verified_at' => $apiUser['email_verified_at'],
+                    //     'profile' => $apiUser['profile'],
+                    //     'max_score' => $apiUser['max_score'],
+                    //     'status' => $apiUser['status'],
+                    //     'is_changed_password' => $apiUser['is_changed_password'],
+                    //     'agent_id' => $apiUser['agent_id'],
+                    //     'payment_type_id' => $apiUser['payment_type_id'],
+                    //     'agent_logo' => $apiUser['agent_logo'],
+                    //     'account_name' => $apiUser['account_name'],
+                    //     'account_number' => $apiUser['account_number'],
+                    //     'line_id' => $apiUser['line_id'],
+                    //     'commission' => $apiUser['commission'],
+                    //     'referral_code' => $apiUser['referral_code'],
+                    //     'created_at' => $apiUser['created_at'],
+                    //     'updated_at' => $apiUser['updated_at'],
+                    // ]);
                 }
             }
 
